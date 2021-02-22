1b2ec348e6738cff292a9573ff73e5115b3406d9
waddweb <info.webdev@bk.ru>
2021-02-23 00:19:22 +0200

    file view.php updated

--- view.php
+++ view.php
@@ -14,7 +14,7 @@ $comments = mysqli_fetch_all($result);
 ?>
 
 <!DOCTYPE html>
-<html lang="en">g
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
